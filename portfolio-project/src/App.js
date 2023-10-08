22b8f2a07463f1887e5a651519e13e2d8cc6ede5
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-10-08 18:26:59 -0500

    navbar v1

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHome, faEnvelope, faCompass, faPlusSquare, faUser, faSearch} from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Routes>
-
-      </Routes>
+      <Navbar/>      
     </>
   );
 }
--- portfolio-project/src/components/Navbar.js
+++ portfolio-project/src/components/Navbar.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import {NavLink} from 'react-router-dom'
+import './Navbar.css'
+import {AiOutlineHome, AiOutlineUser, AiOutlineFundProjectionScreen, AiOutlineFileText} from 'react-icons/ai'
+import {GrBlog} from 'react-icons/gr'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar_wrapper">
+      <nav className='navbar'>
+         <div className="navbar-logo_wrapper">
+            <span className='navabar-logo'>O.O</span>
+         </div>
+         <div className="navbar_icons">
+            <NavLink className="navbar_link" to="/main_feed" activeClassName="active-link">
+                <AiOutlineHome/> Home
+            </NavLink>
+            <NavLink className="navbar_link" to="/about" activeClassName="active-link">
+                <AiOutlineUser/> About
+            </NavLink>
+            <NavLink className="navbar_link" to="/projects" activeClassName="active-link">
+                <AiOutlineFundProjectionScreen/> Projects
+            </NavLink>
+            <NavLink className="navbar_link" to="/resume" activeClassName="active-link">
+                <AiOutlineFileText/> Resume
+            </NavLink>
+            <NavLink>
+                <GrBlog/> Blog
+            </NavLink>
+         </div>
+      </nav>
+    </div>
   )
 }
 
