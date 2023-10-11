759af2c05156478e67230c9ca24ddb821557c419
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-10-11 14:17:20 -0500

    navbar v2

--- portfolio-project/src/components/Navbar.js
+++ portfolio-project/src/components/Navbar.js
@@ -1,14 +1,14 @@
 import {NavLink} from 'react-router-dom'
 import './Navbar.css'
 import {AiOutlineHome, AiOutlineUser, AiOutlineFundProjectionScreen, AiOutlineFileText} from 'react-icons/ai'
-import {GrBlog} from 'react-icons/gr'
+import {ImBlog} from 'react-icons/im'
 
 function Navbar() {
   return (
     <div className="navbar_wrapper">
       <nav className='navbar'>
          <div className="navbar-logo_wrapper">
-            <span className='navabar-logo'>O.O</span>
+            <span className='navabar-logo'>Oo.</span>
          </div>
          <div className="navbar_icons">
             <NavLink className="navbar_link" to="/main_feed" activeClassName="active-link">
@@ -23,8 +23,8 @@ function Navbar() {
             <NavLink className="navbar_link" to="/resume" activeClassName="active-link">
                 <AiOutlineFileText/> Resume
             </NavLink>
-            <NavLink>
-                <GrBlog/> Blog
+            <NavLink className="navbar_link" to="/blog" activeClassName="active-link">
+                <ImBlog/> Blog
             </NavLink>
          </div>
       </nav>
