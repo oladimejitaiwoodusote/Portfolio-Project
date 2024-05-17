15aea1868be54761886e79aa55b6ccdac81acedc
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2024-05-17 06:50:42 -0500

    Updated Project page

--- portfolio-project/src/components/Projects.js
+++ portfolio-project/src/components/Projects.js
@@ -1,33 +1,42 @@
 import React from 'react'
-import projImg1 from '../Assets/instagram.jpg'
-import projImg2 from '../Assets/collectorsgramm.jpg'
-import projImg3 from '../Assets/pet_appointment.jpg'
+import projImg1 from '../Assets/Spotify.jpg'
+import projImg2 from '../Assets/instagram.jpg'
+import projImg3 from '../Assets/collectorsgramm.jpg'
+import projImg4 from '../Assets/pet_appointment.jpg'
 import './Projects.css'
 
 const projects = [
   {
     id: 1,
-    name: 'Instagram Clone',
-    description: 'An instagram clone application built using react.js for the front-end, Flask for the backend and PostgreSQL for storage. Implemented user authentication and authorization for account management. Also implemented social media features for user interactivity.',
-    githubLink: 'https://github.com/oladimejitaiwoodusote/Instagram-Mock',
-    demoLink: 'https://instagram-mock-one.vercel.app/',
+    name: 'Spotify Clone',
+    description: 'A full-stack application that emulates key functionalities of Spotify, allowing users to listen to songs, create playlists, and manage a music library. Built with Next.js, this project integrates Stripe for secure payment handling, uses Supabase for backend services, and PostgreSQL for database management. It provides both free and premium user experiences with Tailwind CSS for a sleek, responsive design.',
+    githubLink: 'https://github.com/oladimejitaiwoodusote/Spotify-Mock',
+    demoLink: 'https://spotify-mock-xi.vercel.app/',
     image: projImg1
   },
   {
     id: 2,
-    name: 'Collectorsgramm',
-    description: 'A mock social media site for vinyl collectors built using react.js for the front-end, Flask for the backend and SQLite for storage. Implemented user authentication and authorization for account management. Also implemented social media features for user interactivity.',
-    githubLink: 'https://github.com/oladimejitaiwoodusote/Capstone-',
-    demoLink: '',
+    name: 'Instagram Clone',
+    description: 'An instagram clone application built using react.js for the front-end, Flask for the backend and PostgreSQL for storage. Implemented user authentication and authorization for account management. Also implemented social media features for user interactivity.',
+    githubLink: 'https://github.com/oladimejitaiwoodusote/Instagram-Mock',
+    demoLink: 'https://instagram-mock-one.vercel.app/',
     image: projImg2
   },
   {
     id: 3,
+    name: 'Collectorsgramm',
+    description: 'A mock social media site for vinyl collectors built using react.js for the front-end, Flask for the backend and SQLite for storage. Implemented user authentication and authorization for account management. Also implemented social media features for user interactivity.',
+    githubLink: 'https://github.com/oladimejitaiwoodusote/Capstone-',
+    demoLink: '',
+    image: projImg3
+  },
+  {
+    id: 4,
     name: 'Pet Appointment Tracker',
     description: 'Developed an appointment tracker for pet owners and doctors using react.js for the front-end, Flask for the backend and SQLite for storage. Implemented user authentication and authorization for account management.',
     githubLink: 'https://github.com/oladimejitaiwoodusote/phase-4-project',
     demoLink: '',
-    image: projImg3
+    image: projImg4
   }
 ]
 
